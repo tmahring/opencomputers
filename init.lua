b5251ba9bce2740cbd920e4db749829ff0dc9847
Thomas Mahringer <tmahring@tmweb.at>
2017-05-15 21:54:30 +0200

    asdf

---
+++ init.lua
@@ -0,0 +1,8 @@
+local component = require("component")
+local fs = require("filesystem")
+local internet = require("internet")
+local shell = require("shell")
+
+local baseurl = "http://github.com/tmahring/opencomputers"
+
+io.write("FOOBAR.\n")
